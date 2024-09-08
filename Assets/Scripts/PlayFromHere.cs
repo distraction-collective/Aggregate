631beeb5981f4019e07e79799aa554fba2e68722
Nino Filiu <nino.filiu@gmail.com>
2024-09-08 19:47:29 +0200

    fix: PlayFromHere takes into account the respawn

--- Assets/Scripts/PlayFromHere.cs
+++ Assets/Scripts/PlayFromHere.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DesirePaths;
 using RootMotion.Dynamics;
 using StarterAssets;
 using UnityEngine;
@@ -7,10 +8,13 @@ using UnityEngine;
 public class PlayFromHere : MonoBehaviour {
   public ThirdPersonController player;
   public PuppetMaster puppetMaster;
+  public PlayerHealth health;
   void Start() {
 
     player.transform.position = transform.position;
     player.transform.rotation = transform.rotation;
     puppetMaster.Teleport(transform.position, transform.rotation, true);
+    health.respawn_position = transform.position;
+    health.respawn_rotation = transform.rotation;
   }
 }
