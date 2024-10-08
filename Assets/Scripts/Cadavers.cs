4d44d69c2540a65377c5419a9fcc35261db2cb2f
Nino Filiu <nino.filiu@gmail.com>
2024-10-08 02:23:31 +0200

    fix: deposit cadaver on ground

--- Assets/Scripts/Cadavers.cs
+++ Assets/Scripts/Cadavers.cs
@@ -1,4 +1,3 @@
-
 using StarterAssets;
 using UnityEngine;
 
@@ -6,9 +5,13 @@ public class Cadavers : MonoBehaviour {
   public GameObject[] corpses;
   public ThirdPersonController player;
   public void DepositCadaver() {
-
     int index = Random.Range(0, corpses.Length);
     GameObject corpse = corpses[index];
-    Instantiate(corpse, player.transform.position, Quaternion.identity);
+    RaycastHit hitInfo;
+    bool isHit = Physics.Raycast(player.transform.position, Vector3.down,
+                                 out hitInfo, Mathf.Infinity);
+    Vector3 pos = isHit ? hitInfo.point : player.transform.position;
+    Debug.Log($"{isHit} {hitInfo.point} {player.transform.position}");
+    Instantiate(corpse, pos, Quaternion.identity);
   }
 }
